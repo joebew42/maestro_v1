801d4f682c47d27a7b19a6674b62d0c5e925ca25
JoeBew42 <joebew42@gmail.com>
2014-08-15 18:20:28 +0200

    Adding the list of dependencies to service

--- main.py
+++ main.py
@@ -13,26 +13,25 @@ class JSONParser:
 
     def __init__(self, json_text):
         self.__json = json.loads(json_text)
-        self.__services = {}
-        self.__dependencies = []
-        self.__parse()
+        self.__services = self.__load_services(self.__json)
+        self.__dependencies = self.__load_dependencies(self.__services)
 
-    def __parse(self):
-        self.__load_services()
-        self.__load_dependencies()
-
-    def __load_services(self):
-        for item in self.__json:
+    def __load_services(self, json):
+        services = {}
+        for item in json:
             name = item['name']
             policy = item.get('restart', RestartPolicy.NONE)
-            self.__services[name] = Service(name, item['command'], policy)
-        logging.info("JSONPARSER >> Resolved services: {}".format(self.services()))
+            services[name] = Service(name, item['command'], policy, item.get('requires', []))
+        logging.info("JSONPARSER >> Resolved services: {}".format(services.values()))
+        return services
 
-    def __load_dependencies(self):
-        for item in self.__json:
-            for dependency in item.get('requires', []):
-                self.__dependencies.append((self.__services[item['name']], self.__services[dependency]))
-        logging.info("JSONPARSER >> Resolved dependencies: {}".format(self.dependencies()))
+    def __load_dependencies(self, services):
+        dependencies = []
+        for service in services.values():
+            for dependency_name in service.dependencies():
+                dependencies.append((service, services[dependency_name]))
+        logging.info("JSONPARSER >> Resolved dependencies: {}".format(dependencies))
+        return dependencies
 
     def services(self):
         return self.__services.values()
@@ -130,11 +129,12 @@ class RestartPolicy:
 # # # SERVICE # # #
 
 class Service:
-    def __init__(self, name, command, policy=RestartPolicy.NONE):
+    def __init__(self, name, command, policy=RestartPolicy.NONE, dependencies=[]):
         self.__name = name
         self.__command = command
         self.__process = None
         self.__policy = policy
+        self.__dependencies = dependencies
 
     def name(self):
         return self.__name
@@ -144,6 +144,9 @@ class Service:
 
     def policy(self):
         return self.__policy
+
+    def dependencies(self):
+        return self.__dependencies
 
     def pid(self):
         return self.__process.pid
