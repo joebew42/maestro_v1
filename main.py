87f0c712951a162439cc144a9cbc3eef4816a882
JoeBew42 <joebew42@gmail.com>
2014-11-10 17:24:13 +0100

    Run docker container with interactive option

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ from os.path import exists as os_path_exists
 class OSProcessDockerThread(OSProcessThread):
     def _spawn_process(self):
         self.__cid_file_path = "docker_cids/{0}".format(self._service.name())
-        docker_cmd = ["docker", "run", "-t", "--rm=true", "--cidfile=\"{0}\"".format(self.__cid_file_path), "--name=\"{0}\"".format(self._service.name())]
+        docker_cmd = ["docker", "run", "-i", "-t", "--rm=true", "--cidfile=\"{0}\"".format(self.__cid_file_path), "--name=\"{0}\"".format(self._service.name())]
 
         for volumes_from in self._service.params('volumes_from'):
             docker_cmd += ["--volumes-from=\"{0}\"".format(volumes_from)]
